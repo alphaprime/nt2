417a60bf542982cf68bdc9a3707bff7e0f484d9d
jtlap <jtlapreste@gmail.com>
2013-08-21 17:37:13 +0200

    correcting/polishing  abs, fabs and fabsf are better in scalar for float and double

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/abs.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/abs.hpp
@@ -21,31 +21,7 @@ namespace boost { namespace simd { namespace ext
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return (a0 >= 0)?a0:-a0;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abs_, tag::cpu_
-                            , (A0)
-                            , (scalar_< arithmetic_<A0> >)
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
-    {
-      return  (a0 >= 0)?a0:-a0;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abs_, tag::cpu_
-                            , (A0)
-                            , (scalar_< int32_<A0> >)
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
-    {
-      A0 mask = a0 >> (sizeof(int32_t)*8 - 1);
+      A0 mask = a0 >> (sizeof(result_type)*8 - 1);
       return (a0 + mask) ^ mask;
     }
   };
@@ -58,10 +34,21 @@ namespace boost { namespace simd { namespace ext
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-       return ::fabs(a0);
+       return ::fabsf(a0);
     }
   };
 
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abs_, tag::cpu_
+                            , (A0)
+                            , (scalar_< double_<A0> >)
+                            )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+       return ::fabs(a0);
+    }
+  };
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abs_, tag::cpu_
                             , (A0)
                             , (scalar_< unsigned_<A0> >)
@@ -79,7 +66,7 @@ namespace boost { namespace simd { namespace ext
                             , (scalar_< bool_<A0> >)
                             )
   {
-    typedef A0 result_type;
+    typedef A0& result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
       return a0;
